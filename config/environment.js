d52967e4489980d7f428b398d125db63255ff6dc
Michael Shafir <michael.shafir@gmail.com>
2018-06-11 22:44:43 -0400

    Create starter application with some good starting addons

--- config/environment.js
+++ config/environment.js
@@ -3,6 +3,7 @@
 module.exports = function(environment) {
   let ENV = {
     modulePrefix: 'starterapp',
+		podModulePrefix: 'starterapp/pods',
     environment,
     rootURL: '/',
     locationType: 'auto',
